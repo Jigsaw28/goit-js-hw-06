ca5a2421164b54b9b4b5e9ce45ca3dca375d991d
Jigsaw28 <levenko7@gmail.com>
2023-02-27 00:11:16 +0200

    зроблена 10 задача

--- js/task-10.js
+++ js/task-10.js
@@ -5,16 +5,33 @@ function getRandomHexColor() {
 }
 
 const box = document.querySelector('#boxes');
-const btnDestroy = document.querySelector('button[data-create');
-const btnCreate = document.querySelector('button[data-create');
-const input = document.querySelector('input');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const btnCreate = document.querySelector('button[data-create]');
+const input = document.querySelector('input[type = "number"]');
 
 
 
+btnCreate.addEventListener('click', onBtnClickCreateAmount);
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function onBtnClickCreateAmount() {
+  createBoxes(input.value) 
+}
 
 function createBoxes(amount) {
-  const createBox = document.createElement('div');
-  const currentBoxes = createBox * amount;
-  return box.prepend(createBox)
+  const arrayBox = [];
+  for(let i = 1; i <= amount; i++) {
+    const boxSize = 20;
+    const createBox = document.createElement('div');
+    createBox.style.width = `${boxSize + i*10}px`;
+    createBox.style.height = `${boxSize + i*10}px`;
+    createBox.style.backgroundColor = getRandomHexColor();
+    arrayBox.push(createBox);
+  }
+  box.prepend(...arrayBox);
 }
- console.log(createBoxes(4))
+
+function destroyBoxes() {
+  box.innerHTML = "";
+}
+
